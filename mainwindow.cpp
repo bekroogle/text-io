f6980e1f50c1b73fcd98ed703bed80c799355022
Benjamin Kruger <ben@KDE.(none)>
2012-07-28 02:37:15 -0500

    Added Comments.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,25 +15,30 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_action_New_triggered()
 {
+    //Clear the page.
     ui->plainTextEdit->clear();
 }
 
 void MainWindow::on_action_Open_triggered()
 {
+    // Get a file name from an Open file dialog.
     QString fileName = QFileDialog::getOpenFileName(this,
                                                     "Open Configuration File.",
                                                     "/home",
                                                     "All Files (*.*)");
-    qDebug() << fileName;
+    // Open the file
     QFile *inFile = new QFile(fileName);
 
     if (!inFile->open(QIODevice::ReadOnly | QIODevice::Text))
         return;
 
+    // Write the file to a QString
     QString buf;
+
     while (!inFile->atEnd()) {
         buf.append(inFile->readLine());
     }
 
+    // Put the QString into the text editor window.
     ui->plainTextEdit->insertPlainText(buf);
 }
